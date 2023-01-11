5b6f0d88511080995fca0dd1f192dc8c5dc442c7
and22122 <122041196+and22122@users.noreply.github.com>
2023-01-11 16:37:42 -0700

    I wrote the programs for C# Prep 1 and Prep 2.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fn = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string ln = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.Write("Your name is ");
+        Console.Write(ln);
+        Console.Write(", ");
+        Console.Write(fn);
+        Console.Write(" ");
+        Console.Write(ln);
+        Console.WriteLine(".");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string gp = Console.ReadLine();
+        int gpn1 = int.Parse(gp) / 10;
+        int gpn2 = int.Parse(gp) - 10 * gpn1;
+        int gpn = 10 * gpn1 + gpn2;
+
+        Console.Write("Your grade was ");
+
+        char letter;
+
+        if (gpn >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gpn >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gpn >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gpn >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        string sign = "";
+
+        if (gpn2 >= 7)
+        {
+            sign = "+";
+        }
+        else if (gpn2 < 3) {
+            sign = "-";
+        }
+
+        if ((letter == 'A' && sign == "+") || (letter == 'F'))
+        {
+            sign = "";
+        }
+
+        string grade = letter.ToString() + sign;
+
+        if (letter == 'A' || letter == 'F')
+        {
+            Console.Write("an ");
+        }
+        else {
+            Console.Write("a ");
+        }
+
+        Console.Write(grade);
+
+        Console.Write(" which means you ");
+
+        if (gpn >= 70)
+        {
+            Console.Write("passed! Good job!");
+        }
+        else
+        {
+            Console.Write("failed. Nice try!");
+        }
     }
 }
