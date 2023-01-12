a2989c4f9d761450f68220b096c95be1ded24d53
and22122 <122041196+and22122@users.noreply.github.com>
2023-01-12 15:42:33 -0700

    I programmed csharp-prep Prep3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string keepPlaying = "yes";
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess;
+            int guesses = 0;
+            
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                guesses ++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+            } while (!(magicNumber == guess));
+
+            Console.Write("You guessed it in ");
+            Console.Write(guesses);
+            Console.WriteLine(" tries!");
+
+            Console.Write("Play again? ");
+            keepPlaying = Console.ReadLine();
+        } while (keepPlaying == "yes");
     }
 }
