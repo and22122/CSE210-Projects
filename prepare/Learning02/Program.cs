081486a0f04615961449b01b418d7de818130d7b
and22122 <122041196+and22122@users.noreply.github.com>
2023-01-21 17:07:33 -0700

    I wrote the programs for prepare Learning02.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2000;
+        job1._endYear = 2010;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Web Developer";
+        job2._startYear = 2012;
+        job2._endYear = 2015;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
