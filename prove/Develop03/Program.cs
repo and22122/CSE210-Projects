c43c02a7dea8d14bd18d3e585369459c6c579ea6
and22122 <122041196+and22122@users.noreply.github.com>
2023-02-09 16:36:09 -0700

    Worked on Scripture Memorizer.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,27 +3,30 @@ using System.Collections.Generic;
 
 class Program
 {
-    scripture scrip = new scripture();
-
-    bool repeatloop = true;
-
-    do
+    static void Main(string[] args)
     {
-        System.Console.Write("Do you want to continue? type Enter to continue; type quit to quit");
-        string input = System.Console.ReadLine();
-        
-        if (input == 'quit')
-        {
-            repeatloop = false;
-        }
-        else if (input == "")
-        {
-            repeatloop = true;
-        }
+        Scripture scrip = new Scripture();
 
-        if (scrip.AllAreHidden())
+        bool repeatloop = true;
+
+        do
         {
-            repeatloop = false;
-        }
-    } while (repeatloop);
+            System.Console.Write("Do you want to continue? type Enter to continue; type quit to quit");
+            string input = System.Console.ReadLine();
+            
+            if (input == "quit")
+            {
+                repeatloop = false;
+            }
+            else if (input == "")
+            {
+                repeatloop = true;
+            }
+
+            if (scrip.AllAreHidden())
+            {
+                repeatloop = false;
+            }
+        } while (repeatloop);
+    }
 }
