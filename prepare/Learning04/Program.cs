fcccbd8fefc6af6ee252a4539dab45cf0acffeaf
and22122 <122041196+and22122@users.noreply.github.com>
2023-02-18 17:28:13 -0700

    Did the Learn 04 Assignment.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment m1 = new MathAssignment("Jalen Anderson", "Math", "1.2", "3-8");
+        WritingAssignment w1 = new WritingAssignment("Jalen Anderson", "Writing", "Essay 1");
+
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
